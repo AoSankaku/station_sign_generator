075aa3f5c2196ebf5f1a08d8d7346c51163e84a7
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-08-08 22:50:07 +0900

    Removed default contents

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
+import { Rect, Layer, Stage } from 'react-konva'
+import styled from 'styled-components'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SignWrapper>
+        <Stage width={100} height={100}>
+          <Layer>
+            <Rect fill='white' x={0} y={0} width={100} height={100} />
+            <Rect fill='red' x={2} y={2} width={60} height={30} />
+          </Layer>
+        </Stage>
+      </SignWrapper>
+      <Button variant="contained">unchiburi</Button>
     </>
   )
 }
+
+const SignWrapper = styled.div`
+width: 100%;
+background: blue;
+`
 
 export default App
