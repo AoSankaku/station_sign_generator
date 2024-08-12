d9c2a32693029fc84457b6d42e02eaaf1ee7e5c4
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-08-13 00:56:44 +0900

    Bulk update on fonts

--- src/components/signs/JrEastSign.tsx
+++ src/components/signs/JrEastSign.tsx
@@ -6,11 +6,13 @@ import styled from 'styled-components'
 import Konva from "konva"
 import processStationNumber, { processedStationNumber } from "../../functions/processStationNumber"
 
+import '../../assets/css/fonts.css'
+
 const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<Konva.Stage>) => {
 
   //const stageRef = useRef<Konva.Stage>(null)
 
-  const { stationName, stationNameEnglish, stationNameFurigana, stationNameChinese, stationNameKorean, stationNote, leftStationName, leftStationNameEnglish, rightStationName, rightStationNameEnglish, stationNumber, baseColor, lineColor, direction, ratio } = props
+  const { stationName, stationNameEnglish, stationNameFurigana, stationNameChinese, stationNameKorean, stationNote, leftStationName, leftStationNameEnglish, rightStationName, rightStationNameEnglish, stationNumber, stationThreeLetterCode, baseColor, lineColor, direction, ratio } = props
   const getSpacedStationName = () => {
     const str = stationName
     switch (str.length) {
@@ -25,6 +27,7 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
   const lineHeight = 24
   const linePosY = 70
   const [processedStationNumber, setProcessedStationNumber] = useState<processedStationNumber>({});
+  const [isFontLoaded, setIsFontLoaded] = useState(false)
 
   useEffect(() => {
     setWidth(height * ratio);
@@ -38,6 +41,15 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
     }
   }, [stationNumber])
 
+  useEffect(() => {
+    document.fonts.ready.then((fontFaceSet) => {
+      const fontFaces = [...fontFaceSet]
+      console.dir(fontFaces)
+      console.log('All fonts have been loaded.')
+      setIsFontLoaded(true)
+    })
+  }, [])
+
   const autoSpace = (str: string) => {
     return str.length <= 2 ? str.split('').join(' ') : str;
   }
@@ -46,7 +58,8 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
     const tempText = new Konva.Text({
       text: getSpacedStationName(),
       fontSize: 32,
-      fontFamily: 'Noto Sans Japanese',
+      fontFamily: 'NotoSansJP',
+      fontStyle: '900',
     });
     return tempText.getWidth()
   }
@@ -63,40 +76,40 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
             direction == 'left' &&
             <>
               <Rect fill={baseColor} x={startingPoint} y={linePosY} width={width} height={lineHeight} />
-              <Text text={autoSpace(rightStationName)} width={width} x={-30} y={74} fontSize={15} fontStyle='500' fontFamily='Noto Sans Japanese' fill='white' align='right' />
-              <Text text={autoSpace(rightStationNameEnglish)} width={width} x={-30} y={100} fontSize={12} fontStyle='300' fontFamily='Helvetica Neue' fill='black' align='right' />
+              <Text text={autoSpace(rightStationName)} width={width} x={-30} y={74} fontSize={15} fontStyle='500' fontFamily='NotoSansJP' fill='white' align='right' />
+              <Text text={rightStationNameEnglish} width={width} x={-30} y={100} fontSize={12} fontFamily='OverusedGrotesk' fill='black' align='right' />
             </>
           }
           {
             direction == 'right' &&
             <>
               <Rect fill={baseColor} x={0} y={linePosY} width={width - 80} height={lineHeight} />
-              <Text text={autoSpace(leftStationName)} width={width} x={30} y={74} fontSize={15} fontStyle='500' fontFamily='Noto Sans Japanese' fill='white' align='left' />
-              <Text text={autoSpace(leftStationNameEnglish)} width={width} x={30} y={100} fontSize={12} fontStyle='300' fontFamily='Helvetica Neue' fill='black' align='left' />
+              <Text text={autoSpace(leftStationName)} width={width} x={30} y={74} fontSize={15} fontStyle='500' fontFamily='NotoSansJP' fill='white' align='left' />
+              <Text text={leftStationNameEnglish} width={width} x={30} y={100} fontSize={12} fontFamily='OverusedGrotesk' fill='black' align='left' />
             </>
           }
           {
             (direction == 'left' || direction == 'both') &&
-            <Text text={autoSpace(leftStationName)} width={width} x={60} y={72} fontSize={21} fontStyle='500' fontFamily='Noto Sans Japanese' fill='white' align='left' />
+            <Text text={autoSpace(leftStationName)} width={width} x={60} y={72} fontSize={21} fontStyle='400' fontFamily='NotoSansJP' fill='white' align='left' />
           }
           {
             (direction == 'both' || direction == 'right') &&
-            <Text text={autoSpace(rightStationName)} width={width} x={-60} y={72} fontSize={21} fontStyle='500' fontFamily='Noto Sans Japanese' fill='white' align='right' />
+            <Text text={autoSpace(rightStationName)} width={width} x={-60} y={72} fontSize={21} fontStyle='400' fontFamily='NotoSansJP' fill='white' align='right' />
           }
           <Rect stroke='grey' strokeWidth={8} x={0} y={0} width={width} height={height} />
           <Rect fill={lineColor} x={width / 2 - 12} y={70} width={24} height={24} />
-          <Text text={getSpacedStationName()} width={width} x={0} y={14} fontSize={32} fontStyle='600' fontFamily='Noto Sans Japanese' fill='black' align='center' />
+          <Text text={getSpacedStationName()} width={width} x={0} y={16} fontSize={32} fontFamily='NotoSansJP' fontStyle="900" fill='black' align='center' />
           {processedStationNumber.prefix &&
             <>
-              <Rect stroke={lineColor} strokeWidth={4} x={-45 + (width - getStationNameWidth()) / 2} y={15} width={30} height={33} cornerRadius={6} />
-              <Text text={processedStationNumber.prefix} fill='black' x={-45 + (width - getStationNameWidth()) / 2} fontSize={10} fontFamily='Helvetica Neue' y={20} width={30} height={30} align="center" />
-              <Text text={processedStationNumber.number} fill='black' x={-45 + (width - getStationNameWidth()) / 2} fontSize={16} fontFamily='Helvetica Neue' fontStyle="500" y={29} width={30} height={32} align="center" />
+              <Rect stroke={lineColor} strokeWidth={4} x={-45 + (width - getStationNameWidth()) / 2} y={16} width={30} height={33} cornerRadius={6} />
+              <Text text={processedStationNumber.prefix} fill='black' x={-45 + (width - getStationNameWidth()) / 2} fontSize={11} fontFamily={'HindSemiBold'} fontStyle="600" y={22} width={30} height={30} align="center" />
+              <Text text={processedStationNumber.number} fill='black' x={-45 + (width - getStationNameWidth()) / 2} fontSize={17} fontFamily={'HindSemiBold'} fontStyle="600" y={32} width={30} height={32} align="center" />
             </>
           }
-          <Text text={stationNameFurigana} width={width} x={0} y={52} fontSize={12} fontStyle='600' fontFamily='Noto Sans Japanese' fill='black' align='center' />
-          <Text text={stationNameChinese} x={10 + (width + getStationNameWidth()) / 2} y={18} fontSize={10} fontStyle='400' fontFamily='Noto Sans' fill='black' align='center' />
-          <Text text={stationNameKorean} x={10 + (width + getStationNameWidth()) / 2} y={34} fontSize={10} fontStyle='400' fontFamily='Noto Sans' fill='black' align='center' />
-          <Text text={stationNameEnglish} width={width} x={0} y={100} fontSize={14} fontStyle='600' fontFamily='Helvetica Neue' fill='black' align='center' />
+          <Text text={stationNameFurigana} width={width} x={0} y={52} fontSize={12} fontStyle='800' fontFamily='NotoSansJP' fill='black' align='center' />
+          <Text text={stationNameChinese} x={10 + (width + getStationNameWidth()) / 2} y={18} fontSize={10} fontStyle='400' fontFamily='NotoSansTC' fill='black' align='center' />
+          <Text text={stationNameKorean} x={10 + (width + getStationNameWidth()) / 2} y={35} fontSize={10} fontStyle='400' fontFamily='NotoSansKR' fill='black' align='center' />
+          <Text text={stationNameEnglish} width={width} x={0} y={98} fontSize={16} fontStyle='600' fontFamily='OverusedGrotesk' fill='black' align='center' />
         </Layer>
       </Stage>
     </SignWrapper>
