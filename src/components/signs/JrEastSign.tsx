5bbfbee6ca25cb0ede15a1ae472fafd4ad058e25
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-08-14 18:04:51 +0900

    Removed unnecessary useeffect

--- src/components/signs/JrEastSign.tsx
+++ src/components/signs/JrEastSign.tsx
@@ -27,7 +27,7 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
     }
   }
   const height = 140;
-  const [width, setWidth] = useState(height * ratio);
+  const width = height * ratio;
   const yOffset = 6;
   const startingPoint = 40;
   const lineHeight = 24
@@ -39,10 +39,7 @@ const JrEastSign = forwardRef<Konva.Stage, StationProps>((props, ref: React.Ref<
   const [stageKey, setStageKey] = useState(0)
   const [windowWidth] = useWindowSize();
   const zoomSize = useZoomSize();
-
-  useEffect(() => {
-    setWidth(height * ratio);
-  }, [ratio])
+  const reversedStationArea = stationArea ? [...stationArea].reverse() : undefined;
 
   useEffect(() => {
     if (stationNumber) {
