dce6e1073e332ea54a984634ee041c1cc8e623be
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-08-28 16:03:32 +0900

    Implemented PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
+import { VitePWA } from 'vite-plugin-pwa';
+import i18next from 'i18next';
+import { ManifestOptions } from 'vite-plugin-pwa';
+
+const generateManifest = (t: Function) => ({
+  name: t('meta.pwa.name'),
+  short_name: t('meta.pwa.short-name'),
+  description: t('meta.description'),
+  theme_color: '#ffffff',
+  icons: [
+    {
+      src: 'android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: 'android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+  ],
+}) as (false | Partial<ManifestOptions> | undefined);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/station_sign_generator/",
 
-  plugins: [react(), ViteYaml()],
-})
+  plugins: [
+    react(),
+    ViteYaml(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: generateManifest(i18next.t),
+    }),
+  ]
+}
+)
