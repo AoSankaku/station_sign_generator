56465c46b81aaea475f1c1516759cb3491e39c87
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-09-04 00:16:02 +0900

    Fixed misspelling on meta

--- src/Head.tsx
+++ src/Head.tsx
@@ -5,16 +5,17 @@ import ogpImage from '/src/assets/images/ogp.png'
 
 const Head = () => {
   const { t, i18n } = useTranslation()
+  const url = document.URL
   return (
     <Helmet
       title={t("meta.title")}
       meta={[
         { name: 'description', content: t("meta.description") },
-        { property: 'og:url', content: document.URL },
+        { property: 'og:url', content: url },
         { property: 'og:type', content: "website" },
-        { property: 'og:title', content: t("meta.ttile") },
+        { property: 'og:title', content: t("meta.title") },
         { property: 'og:description', content: t("meta.description") },
-        { property: 'og:image', content: ogpImage },
+        { property: 'og:image', content: url + ogpImage },
         { property: 'og:site_name', content: t("meta.title") },
         { name: 'twitter:card', content: "summary_large_image" },
         { name: 'twitter:site', content: "@Ao_Sankaku" },
