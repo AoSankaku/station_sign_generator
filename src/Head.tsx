3544d28a9cd3c528ac6611b0304465cfb3918e52
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-09-04 00:09:22 +0900

    Updated ogp

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,6 +1,7 @@
 
 import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet-async'
+import ogpImage from '/src/assets/images/ogp.png'
 
 const Head = () => {
   const { t, i18n } = useTranslation()
@@ -8,7 +9,15 @@ const Head = () => {
     <Helmet
       title={t("meta.title")}
       meta={[
-        { name: 'description', content: t("meta.description") }
+        { name: 'description', content: t("meta.description") },
+        { property: 'og:url', content: document.URL },
+        { property: 'og:type', content: "website" },
+        { property: 'og:title', content: t("meta.ttile") },
+        { property: 'og:description', content: t("meta.description") },
+        { property: 'og:image', content: ogpImage },
+        { property: 'og:site_name', content: t("meta.title") },
+        { name: 'twitter:card', content: "summary_large_image" },
+        { name: 'twitter:site', content: "@Ao_Sankaku" },
       ]}
       htmlAttributes={{ lang: i18n.language }}
     />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Temp from './Temp.tsx'
 import './index.css'
 import "react-color-palette/css";
 import "./i18n/configs"
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Head from './Head.tsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 const darkTheme = createTheme({
   palette: {
@@ -22,10 +24,12 @@ const lang = baseUrl.searchParams.get('lang');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <Head />
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider theme={darkTheme}>
+        <Head />
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
