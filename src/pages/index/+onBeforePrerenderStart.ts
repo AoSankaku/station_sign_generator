6d88f3d0904ffa015705ba1a30bd58e5a3366531
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-09-13 03:45:52 +0900

    Added +onBeforePrerenderStart to create static url with locale

---
+++ src/pages/index/+onBeforePrerenderStart.ts
@@ -0,0 +1,10 @@
+import { languages } from "../../i18n/configs"
+
+export { onBeforePrerenderStart }
+
+const onBeforePrerenderStart = () => {
+  const rawLang = languages
+  const langList = rawLang.filter((e) => e !== "ja")
+  const langUrls = langList.map(e => '/' + e)
+  return ["/", ...langUrls]
+}
